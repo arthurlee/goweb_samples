82ab345ef226e78f1b6174f5e0c5ea08086befc0
arthurlee <boyal2008@gmail.com>
2016-09-25 19:18:11 +0800

    scheme and test scripts

--- ch03/web.go
+++ ch03/web.go
@@ -1,3 +1,8 @@
+// Page 96
+//
+// test script:
+//		curl -d "c=3" -d "url_long=111" -d "url_long=222" "http://127.0.0.1:9090/abc?a=1&b=2"
+
 package main
 
 import (
@@ -6,6 +11,9 @@ import (
 	"net/http"
 	"strings"
 )
+
+// Scheme始终是空的
+// 参考https://github.com/kataras/iris/blob/master/http.go中第327行的实现来间接获取
 
 func sayHelloName(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
@@ -27,7 +35,7 @@ func main() {
 	http.HandleFunc("/", sayHelloName)
 
 	port := ":9090"
-	log.Println("Listen to port ", port)
+	log.Println("Listen to port", port)
 	err := http.ListenAndServe(port, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
